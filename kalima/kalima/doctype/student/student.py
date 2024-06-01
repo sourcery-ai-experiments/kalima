9997a85d21a93c998347376148948c18d619c62e
Abdullah <abdullahalsalih@gmail.com>
2024-06-01 15:03:00 +0300

    password fix

--- kalima/kalima/doctype/student/student.py
+++ kalima/kalima/doctype/student/student.py
@@ -8,9 +8,10 @@ from frappe.model.document import Document
 class Student(Document):
 	def save(doc):
 		# user = frappe.get_doc("User", doc.user)
-		if(doc.password == doc.confirm_password):
-			update_password(doc.user, doc.password)
-			frappe.db.commit()
-		else:
-			frappe.throw("Passwords do not match")
+		if(doc.password != None):
+			if(doc.password == doc.confirm_password):
+				update_password(doc.user, doc.password)
+				frappe.db.commit()
+			else:
+				frappe.throw("Passwords do not match")
 
