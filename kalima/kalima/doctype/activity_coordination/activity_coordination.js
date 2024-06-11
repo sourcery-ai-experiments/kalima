0cc62230a0256acd84a6a1101119bec73768dded
Abdullah <abdullahalsalih@gmail.com>
2024-06-11 12:15:30 +0300

    activity fixes

--- kalima/kalima/doctype/activity_coordination/activity_coordination.js
+++ kalima/kalima/doctype/activity_coordination/activity_coordination.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2024, e2next and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Activity Execution", {
+// frappe.ui.form.on("Activity Coordination", {
 // 	refresh(frm) {
 
 // 	},
--- kalima/kalima/doctype/activity_coordination/test_activity_coordination.py
+++ kalima/kalima/doctype/activity_coordination/test_activity_coordination.py
@@ -5,5 +5,5 @@
 from frappe.tests.utils import FrappeTestCase
 
 
-class TestActivityExecution(FrappeTestCase):
+class TestActivityCoordination(FrappeTestCase):
 	pass
