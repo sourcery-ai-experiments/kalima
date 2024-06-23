6d8522daafe66318a4e269d98b819caf8ada938d
Abdullah <asalih@creativeadvtech.com>
2024-06-23 12:37:47 +0300

    fees fixes

--- kalima/kalima/doctype/students_fees/students_fees.js
+++ kalima/kalima/doctype/students_fees/students_fees.js
@@ -4,7 +4,7 @@ frappe.ui.form.on("Students Fees", {
     refresh(frm) {
         frm.set_query("item", () => ({
             "filters": {
-                "is_stock_item":0,
+                "is_stock_item": 0,
             }
         }));
         frm.add_custom_button(__('Fetch Student'), async function () {
@@ -24,13 +24,24 @@ frappe.ui.form.on("Students Fees", {
             var temp_list = [];
 
             for (let element of students) {
-                var discount_value = await frappe.db.get_value("Constant", element.discount, 'amount');
+                var discount_type = await frappe.db.get_value("Constant", element.discount, 'type');
+                var discount_value;
+                if (discount_type.message.type == "Percentage") {
+                    discount_value = await frappe.db.get_value("Constant", element.discount, 'percentage');
+                    discount_value = frm.doc.fee_amount * (discount_value.message.percentage/100);
+                }
+                else {
+                    discount_value = await frappe.db.get_value("Constant", element.discount, 'amount');
+                    discount_value = discount_value.message.amount;
+
+                }
+
                 temp_list.push({
                     student: element.name,
                     study_type: frm.doc.study_system,
                     discount_type: element.discount,
                     amount: frm.doc.fee_amount,
-                    amount_after_discount: frm.doc.fee_amount - discount_value.message.amount,
+                    amount_after_discount: frm.doc.fee_amount - discount_value,
                 });
             }
             frm.set_value('students_fees', temp_list);
@@ -47,7 +58,44 @@ frappe.ui.form.on("Students Fees", {
         }).addClass('bg-success', 'text-white').css({
             "color": "white",
         });
+        if (frm.doc.docstatus === 1 && (frm.doc.transfer_date == null && frm.doc.transfer_date == undefined)) {
 
+            frm.add_custom_button(__('Create Invoices'), async function () {
+                await frappe.call({
+                    method: 'kalima.kalima.doctype.students_fees.students_fees.create_invoices',
+                    args: {
+                        docname: frm.doc.name
+                    },
+                    callback: function (r) {
+                        if (!r.exc) {
+                            frappe.msgprint(__('Invoices created successfully!'));
+                        }
+                    }
+                });
+
+            }).addClass('bg-secondary', 'text-white').css({
+                "color": "white",
+            });
+        }
+        if (frm.doc.docstatus === 1 && (frm.doc.transfer_date == null && frm.doc.transfer_date == undefined)) {
+
+            frm.add_custom_button(__('Cancel Invoices'), async function () {
+                await frappe.call({
+                    method: 'kalima.kalima.doctype.students_fees.students_fees.cancel_invoices',
+                    args: {
+                        docname: frm.doc.name
+                    },
+                    callback: function (r) {
+                        if (!r.exc) {
+                            frappe.msgprint(__('Invoices Canceled successfully!'));
+                        }
+                    }
+                });
+
+            }).addClass('bg-info', 'text-white').css({
+                "color": "white",
+            });
+        }
     },
 });
 
--- kalima/kalima/doctype/students_fees/students_fees.py
+++ kalima/kalima/doctype/students_fees/students_fees.py
@@ -2,10 +2,13 @@
 from frappe.model.document import Document
 import frappe
 from frappe.model.docstatus import DocStatus
+from frappe.utils import nowdate
 
 
 class StudentsFees(Document):
-	def on_submit(doc):
+	# def on_submit(doc):
+	def create_invoices(doc):
+		# doc = frappe.get_doc("Students Fees",docname)
 		students= doc.students_fees
   
 		for std in students:
@@ -30,8 +33,13 @@ class StudentsFees(Document):
 			new_invoice.insert()
 			new_invoice.submit()
    
+		current_date = nowdate()
+		doc.transfer_date = current_date
+		doc.save()
    
-	def on_cancel(doc):
+   
+	# def on_cancel(doc):
+	def cancel_invoices(doc):
 		all_docs = frappe.db.get_list('Sales Invoice',
 			filters={
 				"docstatus": DocStatus.submitted()
@@ -43,3 +51,19 @@ class StudentsFees(Document):
 			docu = frappe.get_doc("Sales Invoice",d.name)
 			docu.cancel()
 			
+		doc.transfer_date = None
+		doc.save()
+   
+   
+   
+@frappe.whitelist()
+def create_invoices(docname):
+    doc = frappe.get_doc("Students Fees", docname)
+    doc.create_invoices()
+    
+    
+   
+@frappe.whitelist()
+def cancel_invoices(docname):
+    doc = frappe.get_doc("Students Fees", docname)
+    doc.cancel_invoices()
