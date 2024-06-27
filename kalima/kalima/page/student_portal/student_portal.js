ffc87e5cf242b3170c463b5ae820f95c8287c5f8
Abdullah <asalih@creativeadvtech.com>
2024-06-27 13:01:37 +0300

    attendance

--- kalima/kalima/page/student_portal/student_portal.js
+++ kalima/kalima/page/student_portal/student_portal.js
@@ -1,44 +1,22 @@
-var selected_class;
+var selected_student = "Abdullah Alshehab";
 var selectedTeacher;
 var current_class;// = await frappe.db.get_doc("Class", selected_class,fields=["student_list"]);
 var naming_maps = {};
 
-frappe.pages['student-portal'].on_page_load = async function(wrapper) {
-	var page = frappe.ui.make_app_page({
-		parent: wrapper,
-		title: 'Student Portal',
-		single_column: true
-	});
+frappe.pages['student-portal'].on_page_load = async function (wrapper) {
+    var page = frappe.ui.make_app_page({
+        parent: wrapper,
+        title: 'Student Portal',
+        single_column: true
+    });
     var main_template = frappe.render_template('student_portal', {
         teacher_name: "test"
     }, page.main);
     var $container = $(wrapper).find('.layout-main-section');
     $container.html(main_template);
 
-    await teacher_field(page);
+    await get_current_user_student();
     await content_manager();
-}
-
-async function teacher_field(page) {
-    const teacherSelector = frappe.ui.form.make_control({
-        parent: page.wrapper.find('#teacher-holder'),
-        df: {
-            fieldtype: "Link",
-            options: "Student",
-            fieldname: "employee",
-            label: __("Student"),
-            placeholder: __("Student"),
-            // default: "HR-EMP-00001",
-
-            reqd: 1,
-            // change: async () => {
-            //     selectedTeacher = teacherSelector.get_value();
-            //     await class_field(page, selectedTeacher);
-            // }
-        }
-    });
-    teacherSelector.set_value("HR-EMP-00001");
-    teacherSelector.refresh();
 }
 
 async function content_manager(dont_click = false) {
@@ -54,21 +32,26 @@ async function content_manager(dont_click = false) {
 
             contentColumn.innerHTML = ''; // Clear the content column
             var templateName = "basic";//this.textContent.replace(/\s+/g, '-').toLowerCase(); // Convert button text to lowercase and replace spaces with dashes
+            var template = this.textContent.replace(/\s+/g, '-').toLowerCase(); // Convert button text to lowercase and replace spaces with dashes
             var cnt = frappe.render_template(templateName, {}, contentColumn);
             contentColumn.innerHTML = cnt;
 
             // if (templateName != 'student-list' && templateName != 'dissolution') {
             //     createFormDialogNew(templateName);
             // }
+            // console.log("ggggg");
 
-            // if (templateName === 'sessions-list') {
-            //     const columns = [
-            //         { label: 'Title', fieldname: 'title' },
-            //         { label: 'Issue Date', fieldname: 'issue_date' },
-            //         { label: 'Expiration Date', fieldname: 'expiration_date' }
-            //     ];
-            //     await populateTable('Class Session', contentColumn, columns);
-            // } else if (templateName == "continuous-exam-list") {
+            if (template === 'attendance') {
+                const columns = [
+                    { label: 'Date', fieldname: 'date' },
+                    { label: 'Module', fieldname: 'module' },
+                    { label: 'Status', fieldname: 'status' },
+                    { label: 'Leave', fieldname: 'leave' }
+                ];
+                await atteendance(contentColumn, columns);
+                // await populateTable('Student Attendance Entry', contentColumn, columns);
+            }
+            // else if (templateName == "continuous-exam-list") {
             //     const columns = [
             //         { label: 'Title', fieldname: 'title' },
             //         { label: 'Type', fieldname: 'type' },
@@ -115,4 +98,96 @@ async function content_manager(dont_click = false) {
             btn.click();
         });
     }
+}
+
+
+async function get_current_user_student() {
+    let response = await frappe.call({
+        method: 'kalima.utils.utils.get_current_user_student',
+    });
+    console.log("response.message");
+    console.log(response.message);
+
+    if (response.message) {
+        selected_student = response.message.name;
+    }
+}
+
+async function atteendance(container, columns) {
+    // Fetch data from Frappe
+    var data = await frappe.call({
+        method: 'kalima.utils.utils.get_student_attendance',
+        args: {
+            student_name: selected_student
+        },
+        callback: function (r) {
+            if (r.message) {
+                console.log(r.message);
+            }
+        }
+    });
+
+    // Create table elements
+    const table = document.createElement('table');
+    table.classList.add('table', 'border', 'rounded', 'table-hover');
+
+    const thead = document.createElement('thead');
+    const tr = document.createElement('tr');
+
+    const th = document.createElement('th');
+    th.scope = 'col';
+    th.textContent = "#";
+    tr.appendChild(th);
+
+    // Create table header
+    columns.forEach(col => {
+        const th = document.createElement('th');
+        th.scope = 'col';
+        th.textContent = col.label;
+        tr.appendChild(th);
+    });
+
+
+    thead.appendChild(tr);
+    table.appendChild(thead);
+
+    const tbody = document.createElement('tbody');
+
+    // Populate table rows
+    data.message.forEach((row, index) => {
+        console.log("row");
+        console.log(row["leave"]);
+        const tr = document.createElement('tr');
+        tr.classList.add('clickable-row');
+
+        tr.addEventListener('click', () => {
+            frappe.open_in_new_tab = true;
+            frappe.set_route(`/app/${toKebabCase(doctype)}/${row.name}`);
+        });
+
+        const th = document.createElement('th');
+        th.scope = 'row';
+        th.textContent = index + 1;
+        tr.appendChild(th);
+
+        columns.forEach(col => {
+            const td = document.createElement('td');
+            if (col.fieldname != "leave") {
+                td.textContent = row[col.fieldname] || '';
+            } else {
+                if (td.textContent == 0)
+                    td.textContent = "No";
+                else
+                    td.textContent = "No";
+
+            }
+            tr.appendChild(td);
+        });
+
+
+        tbody.appendChild(tr);
+    });
+
+    table.appendChild(tbody);
+    container.appendChild(table);
 }
--- kalima/utils/utils.py
+++ kalima/utils/utils.py
@@ -9,8 +9,32 @@ def get_classes_for_teacher(teacher_name):
 
 
 @frappe.whitelist()
+def get_student_attendance(student_name):
+    # Define the SQL query
+    query = """
+        SELECT sae.*,sad.*
+        FROM `tabStudent Attendance Entry` sae
+        JOIN `tabAttednance` sad
+        ON sae.name = sad.parent
+        WHERE sad.student = %s
+        ORDER BY sae.date DESC
+    """
+    
+    # Execute the SQL query
+    records = frappe.db.sql(query, (student_name,), as_dict=True)
+    # for r in records:
+    return records
+
+
+@frappe.whitelist()
+def get_current_user_student():
+    user = frappe.session.user
+    student = frappe.get_all('Student', filters={'user': user}, fields=['name', ])
+    return student[0] if student else None
+
+@frappe.whitelist()
 def submit_student_results(student_results):
-    print(student_results)
+
     student_results = json.loads(student_results)
     for result in student_results:
         doc = frappe.get_doc({
