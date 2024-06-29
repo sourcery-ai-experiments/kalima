52c861a73a5ee05a8f323a69bbb52bce0b3ac6cf
Abdullah <asalih@creativeadvtech.com>
2024-06-29 10:56:20 +0300

    portal grades

--- kalima/utils/utils.py
+++ kalima/utils/utils.py
@@ -26,6 +26,22 @@ def get_student_attendance(student_name):
     return records
 
 
+
+@frappe.whitelist()
+def get_student_results(student_name):
+    print(student_name)
+
+    # Define the SQL query
+    query = """
+        SELECT ser.*
+        FROM `tabStudent Exam Result` ser
+        WHERE ser.student = %s;
+    """
+    records = frappe.db.sql(query, (student_name,), as_dict=True)
+    print(records)
+    return records
+
+
 @frappe.whitelist()
 def get_current_user_student():
     user = frappe.session.user
