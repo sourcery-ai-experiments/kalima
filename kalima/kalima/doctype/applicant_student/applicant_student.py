14836c8b83397f3a53925abfba2b29befe142cb2
Osama Muhammed <42488643+osama1998H@users.noreply.github.com>
2024-06-29 06:42:51 +0000

    finish

--- kalima/kalima/doctype/applicant_student/applicant_student.py
+++ kalima/kalima/doctype/applicant_student/applicant_student.py
@@ -3,11 +3,12 @@
 
 import frappe
 from frappe.model.document import Document
+
 # from frappe.utils.user import add_user
 
 
 class ApplicantStudent(Document):
-	pass
+    pass
 
 
 @frappe.whitelist()
@@ -20,11 +21,11 @@ def admit_student(doc_name, department, study_system):
 
     # Create a new "Student" document
     student_doc = frappe.new_doc("Student")
-    
+
     # Iterate through fields in the "Applicant Student" document
     for field in applicant_doc.meta.fields:
         fieldname = field.fieldname
-        
+
         # Only set the field if it exists in the "Student" DocType
         if student_meta.has_field(fieldname):
             student_doc.set(fieldname, applicant_doc.get(fieldname))
@@ -33,39 +34,47 @@ def admit_student(doc_name, department, study_system):
     email_prefix = applicant_doc.english_student_full_name.replace(" ", "").lower()
     custom_email_domain = "Kalima.com"
     email = f"{email_prefix}@{custom_email_domain}"
-    
+
     print("email")
     print(applicant_doc.email)
-    print(email if (applicant_doc.email == None or applicant_doc.email == "") else applicant_doc.email)
-    
+    print(
+        email
+        if (applicant_doc.email == None or applicant_doc.email == "")
+        else applicant_doc.email
+    )
+
     # Create a new user
-    user_doc = frappe.get_doc({
-        "doctype": "User",
-        "email": email if (applicant_doc.email == None or applicant_doc.email == "") else applicant_doc.email,
-        "first_name": applicant_doc.first_name,
-        "last_name": applicant_doc.sure_name,
-        "roles": [
-            {
-                "role": "Student"
-            }
-        ]
-    })
+    user_doc = frappe.get_doc(
+        {
+            "doctype": "User",
+            "email": (
+                email
+                if (applicant_doc.email == None or applicant_doc.email == "")
+                else applicant_doc.email
+            ),
+            "first_name": applicant_doc.first_name,
+            "last_name": applicant_doc.sure_name,
+            "roles": [{"role": "Student"}],
+        }
+    )
     user_doc.save()
-    
-    customer = frappe.get_doc({
-        "doctype": "Customer",
-        "customer_name": student_doc.full_name_in_arabic,
-        "customer_type": "Individual",
-        "customer_group": "Individual",
-        "territory": "All Territories",
-        "portal_users": [
-            {
-                "user": user_doc.name
-            }
-        ]
-    })
+
+    customer = frappe.get_doc(
+        {
+            "doctype": "Customer",
+            "customer_name": student_doc.full_name_in_arabic,
+            "customer_type": "Individual",
+            "customer_group": "Individual",
+            "territory": "All Territories",
+            "portal_users": [{"user": user_doc.name}],
+        }
+    )
     customer.insert()
-    student_doc.email = email if (applicant_doc.email == None or applicant_doc.email == "") else applicant_doc.email
+    student_doc.email = (
+        email
+        if (applicant_doc.email == None or applicant_doc.email == "")
+        else applicant_doc.email
+    )
     student_doc.customer = customer.name
     student_doc.final_selected_course = department
     student_doc.study_system = study_system
