4eb6dc3c84f6decf539bcd7e89306427bc7c1efd
Abdullah <asalih@creativeadvtech.com>
2024-06-30 14:10:27 +0300

    New Result Log Structure

--- kalima/kalima/doctype/class_continuous_exam/class_continuous_exam.py
+++ kalima/kalima/doctype/class_continuous_exam/class_continuous_exam.py
@@ -1,9 +1,41 @@
 # Copyright (c) 2024, e2next and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class ClassContinuousExam(Document):
-	pass
+	def after_insert(doc):
+		for std in doc.continuous_exam_result:
+			new_doc = frappe.get_doc({
+				'doctype': 'Student Result Log',
+	
+				'type': 'Class Continuous Exam',
+    
+				'student': std.student_name,
+
+				'title': doc.title,
+				'score': doc.score,
+				'continuous_exam_type': doc.type,
+				'midterm': doc.midterm,
+				'date': doc.date,
+				'cexams': doc.c_exams,
+        		'class': getattr(doc, 'class', None), 
+				'marked_on': doc.marked_on,
+				'percentage': doc.percentage,
+    
+				'student_code': std.student_code,
+				'present': 1 if std.is_absent == 0 else 0,
+
+				'department': std.department,
+				'net_score': std.net_score,
+				'continuous_score': std.score,
+				'description': std.description,
+
+			})
+	
+		
+		new_doc.insert()
+		new_doc.submit()
+
--- kalima/kalima/doctype/student_exam_result/student_exam_result.py
+++ kalima/kalima/doctype/student_exam_result/student_exam_result.py
@@ -1,9 +1,28 @@
 # Copyright (c) 2024, e2next and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class StudentExamResult(Document):
-	pass
+	def on_submit(doc):
+		doc = frappe.get_doc({
+			'doctype': 'Student Result Log',
+   
+			'type': 'Student Exam Result',
+			
+			'prototype': doc.prototype,
+			'student': doc.student,
+			'module': doc.module,
+			'teacher': doc.teacher,
+			
+			'exam_max_result': doc.exam_max_result,
+			'result':doc.result,
+			'status': doc.status,
+			'cheating': doc.cheating,
+			'present': doc.present,
+		})
+		doc.insert()
+		doc.submit()
+
