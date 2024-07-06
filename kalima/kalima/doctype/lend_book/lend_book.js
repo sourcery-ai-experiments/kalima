d3961d118346bac8807e1c9fd2d62c7b4c8f8350
Abdullah <asalih@creativeadvtech.com>
2024-07-06 14:51:36 +0300

    Some V2

--- kalima/kalima/doctype/lend_book/lend_book.js
+++ kalima/kalima/doctype/lend_book/lend_book.js
@@ -1,6 +1,6 @@
 frappe.ui.form.on("Lend Book", {
     refresh(frm) {
-        if (!frm.is_new() && frm.doc.docstatus != 1) {
+        if (!frm.is_new() && frm.doc.docstatus != 1 && frm.doc.is_returned != 1) {
             frm.add_custom_button(__('Return'), async function () {
                 frm.set_value('is_returned', 1);
                 frm.set_value('return_date', frappe.datetime.now_datetime()); // Assign the current date and time
